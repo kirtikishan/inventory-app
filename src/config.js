fd5bd579ec822fdbade14693bdee7c2bffeddd9d
kirti kishan <kirtikishan123@gmail.com>
2019-08-11 15:35:36 +1000

    add aws configs for apigateway and cognito

---
+++ src/config.js
@@ -0,0 +1,12 @@
+export default {
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: " https://n7gqnawcrg.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_3nUakUNoI",
+      APP_CLIENT_ID: "7vqqu3olali397pgm77eukv0n2",
+      IDENTITY_POOL_ID: "us-east-1:e47aa535-a2dd-4e02-8b17-1c19b6ed1109"
+    }
+  };
